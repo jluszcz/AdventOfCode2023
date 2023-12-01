79f145809560185d0eafb21beebcb3130c2f1dd4
Jacob Luszcz <jacob.luszcz@gmail.com>
2023-12-01 17:39:57 -0500

    Day 1, part 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::FromStr;
+
+use anyhow::{anyhow, Result};
+use log::info;
+
+#[derive(Debug)]
+struct CalibrationValue(u32);
+
+impl FromStr for CalibrationValue {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut val = String::new();
+
+        for c in s.chars() {
+            if c.is_numeric() {
+                val.push(c);
+                break;
+            }
+        }
+
+        for c in s.chars().rev() {
+            if c.is_numeric() {
+                val.push(c);
+                break;
+            }
+        }
+
+        if val.len() != 2 {
+            return Err(anyhow!("Not enough digits in {}", s));
+        }
+
+        let val = u32::from_str(&val)?;
+        Ok(CalibrationValue(val))
+    }
+}
+
+fn main() -> Result<()> {
+    let result: u32 = util::input()?
+        .into_iter()
+        .map_while(|s| CalibrationValue::from_str(&s).ok())
+        .map(|c| c.0)
+        .sum();
+
+    info!("Result: {}", result);
+
+    Ok(())
 }
